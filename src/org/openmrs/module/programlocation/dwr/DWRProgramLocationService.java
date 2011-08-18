8dd2b77c0d5fca92a769c0c2e1b754f7156057cc
cneumann <cneumann@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2011-08-18 13:15:57 +0000

    code style

--- src/org/openmrs/module/programlocation/dwr/DWRProgramLocationService.java
+++ src/org/openmrs/module/programlocation/dwr/DWRProgramLocationService.java
@@ -25,21 +25,20 @@ import org.openmrs.api.context.Context;
 import org.openmrs.util.OpenmrsUtil;
 
 public class DWRProgramLocationService {
-	
+
 	protected final Log log = LogFactory.getLog(getClass());
-	
+
 	DateFormat ymdDf = new SimpleDateFormat("yyyy-MM-dd");
-	
-	public void updatePatientProgram(
-				Integer patientProgramId, 
-				String enrollmentDateYmd, 
-				String completionDateYmd,
-				Integer locationId)
-    throws ParseException {
-		org.openmrs.module.programlocation.PatientProgram pp = (org.openmrs.module.programlocation.PatientProgram)Context.getProgramWorkflowService().getPatientProgram(patientProgramId);
+
+	public void updatePatientProgram(Integer patientProgramId,
+			String enrollmentDateYmd, String completionDateYmd,
+			Integer locationId) throws ParseException {
+		org.openmrs.module.programlocation.PatientProgram pp = (org.openmrs.module.programlocation.PatientProgram) Context
+				.getProgramWorkflowService()
+				.getPatientProgram(patientProgramId);
 		Location loc = null;
-		if(locationId != null) {
-			loc	= Context.getLocationService().getLocation(locationId);
+		if (locationId != null) {
+			loc = Context.getLocationService().getLocation(locationId);
 		}
 		Date dateEnrolled = null;
 		Date dateCompleted = null;
@@ -48,39 +47,41 @@ public class DWRProgramLocationService {
 		Location ppLocation = null;
 		// If persisted date enrolled is not null then parse to ymdDf format.
 		if (null != pp.getDateEnrolled()) {
-		String enrolled = ymdDf.format(pp.getDateEnrolled());
-		if (null != enrolled && enrolled.length() > 0)
-		ppDateEnrolled = ymdDf.parse(enrolled);
+			String enrolled = ymdDf.format(pp.getDateEnrolled());
+			if (null != enrolled && enrolled.length() > 0)
+				ppDateEnrolled = ymdDf.parse(enrolled);
 		}
-		if(null != pp.getLocation()) {
+		if (null != pp.getLocation()) {
 			ppLocation = pp.getLocation();
 		}
 		// If persisted date enrolled is not null then parse to ymdDf format.
 		if (null != pp.getDateCompleted()) {
-		String completed = ymdDf.format(pp.getDateCompleted());
-		if (null != completed && completed.length() > 0)
-		ppDateCompleted = ymdDf.parse(completed);
+			String completed = ymdDf.format(pp.getDateCompleted());
+			if (null != completed && completed.length() > 0)
+				ppDateCompleted = ymdDf.parse(completed);
 		}
 		// Parse parameter dates to ymdDf format.
 		if (enrollmentDateYmd != null && enrollmentDateYmd.length() > 0)
-		dateEnrolled = ymdDf.parse(enrollmentDateYmd);
+			dateEnrolled = ymdDf.parse(enrollmentDateYmd);
 		if (completionDateYmd != null && completionDateYmd.length() > 0)
-		dateCompleted = ymdDf.parse(completionDateYmd);
-		// If either either parameter and persisted instances 
+			dateCompleted = ymdDf.parse(completionDateYmd);
+		// If either either parameter and persisted instances
 		// of enrollment and completion dates are equal, then anyChange is true.
-		boolean anyChange = OpenmrsUtil.nullSafeEquals(dateEnrolled, ppDateEnrolled);
+		boolean anyChange = OpenmrsUtil.nullSafeEquals(dateEnrolled,
+				ppDateEnrolled);
 		anyChange |= OpenmrsUtil.nullSafeEquals(dateCompleted, ppDateCompleted);
 		anyChange |= OpenmrsUtil.nullSafeEquals(loc, ppLocation);
-		
+
 		// Do not update if the enrollment date is after the completion date.
-		if (null != dateEnrolled && null != dateCompleted && dateCompleted.before(dateEnrolled)) {
-		anyChange = false;
+		if (null != dateEnrolled && null != dateCompleted
+				&& dateCompleted.before(dateEnrolled)) {
+			anyChange = false;
 		}
 		if (anyChange) {
-		pp.setDateEnrolled(dateEnrolled);
-		pp.setDateCompleted(dateCompleted);
-		pp.setLocation(loc);
-		Context.getProgramWorkflowService().updatePatientProgram(pp);
+			pp.setDateEnrolled(dateEnrolled);
+			pp.setDateCompleted(dateCompleted);
+			pp.setLocation(loc);
+			Context.getProgramWorkflowService().updatePatientProgram(pp);
 		}
 	}
 }
